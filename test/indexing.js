42304f3640cb446d765f9a35502e10b1e839f0df
Talles L <talleslasmar@gmail.com>
2015-06-26 13:54:38 -0300

    OCD

--- test/indexing.js
+++ test/indexing.js
@@ -4,7 +4,7 @@ var assert   = require('assert')
 
 describe('indexing', function () {
   it('should raise an error if id is not an integer', function () {
-    var redis   = mock.createClient()
+    var redis = mock.createClient()
 
     assert.throws(function () { indexing(redis) })
     assert.throws(function () { indexing(redis, null) })
@@ -16,7 +16,7 @@ describe('indexing', function () {
 
 describe('indexing', function () {
   it('should raise an error if no text is provided', function () {
-    var redis   = mock.createClient()
+    var redis = mock.createClient()
 
     assert.throws(function () { indexing(redis, 1337) })
   })
@@ -24,7 +24,7 @@ describe('indexing', function () {
 
 describe('indexing', function () {
   it('should return false if there\'s nothing to index', function () {
-    var redis   = mock.createClient()
+    var redis = mock.createClient()
 
     assert(!indexing(redis, 1, 'Como?'))
   })
@@ -32,34 +32,34 @@ describe('indexing', function () {
 
 describe('indexing', function () {
   it('should index correctly', function () {
-    var redis   = mock.createClient()
+    var redis = mock.createClient()
 
     assert(indexing(redis, 23, '\
-  É bom\n\
-  Passar uma tarde em Itapuã\n\
-  Ao sol que arde em Itapuã\n\
-  Ouvindo o mar de Itapuã\n\
-  Falar de amor em Itapuã'))
+É bom\n\
+Passar uma tarde em Itapuã\n\
+Ao sol que arde em Itapuã\n\
+Ouvindo o mar de Itapuã\n\
+Falar de amor em Itapuã'))
 
-    assert.deepEqual(redis.db.bom, [ 23 ])
+    assert.deepEqual(redis.db.bom,    [ 23 ])
     assert.deepEqual(redis.db.passar, [ 23 ])
-    assert.deepEqual(redis.db.tarde, [ 23 ])
+    assert.deepEqual(redis.db.tarde,  [ 23 ])
     assert.deepEqual(redis.db.itapua, [ 23 ])
-    assert.deepEqual(redis.db.sol, [ 23 ])
-    assert.deepEqual(redis.db.arde, [ 23 ])
-    assert.deepEqual(redis.db.falar, [ 23 ])
-    assert.deepEqual(redis.db.amor, [ 23 ])
+    assert.deepEqual(redis.db.sol,    [ 23 ])
+    assert.deepEqual(redis.db.arde,   [ 23 ])
+    assert.deepEqual(redis.db.falar,  [ 23 ])
+    assert.deepEqual(redis.db.amor,   [ 23 ])
 
-    assert.deepEqual(redis.db['23:bom'], [ 0 ])
-    assert.deepEqual(redis.db['23:passar'], [ 1 ])
-    assert.deepEqual(redis.db['23:tarde'], [ 2 ])
-    assert.deepEqual(redis.db['23:itapua'], [ 3, 6, 9, 12 ])
-    assert.deepEqual(redis.db['23:sol'], [ 4 ])
-    assert.deepEqual(redis.db['23:arde'], [ 5 ])
-    assert.deepEqual(redis.db['23:ouvindo'], [7])
-    assert.deepEqual(redis.db['23:mar'], [ 8 ])
-    assert.deepEqual(redis.db['23:falar'], [ 10 ])
-    assert.deepEqual(redis.db['23:amor'], [ 11 ])
+    assert.deepEqual(redis.db['23:bom'],     [ 0 ])
+    assert.deepEqual(redis.db['23:passar'],  [ 1 ])
+    assert.deepEqual(redis.db['23:tarde'],   [ 2 ])
+    assert.deepEqual(redis.db['23:itapua'],  [ 3, 6, 9, 12 ])
+    assert.deepEqual(redis.db['23:sol'],     [ 4 ])
+    assert.deepEqual(redis.db['23:arde'],    [ 5 ])
+    assert.deepEqual(redis.db['23:ouvindo'], [ 7 ])
+    assert.deepEqual(redis.db['23:mar'],     [ 8 ])
+    assert.deepEqual(redis.db['23:falar'],   [ 10 ])
+    assert.deepEqual(redis.db['23:amor'],    [ 11 ])
   })
 })
 
--- test/searching.js
+++ test/searching.js
@@ -4,21 +4,21 @@ var assert     = require('assert')
 
 describe('searching', function () {
   it('should match one vocabulary word', function (done) {
-    var redis      = mock.createClient()
+    var redis = mock.createClient()
       , query = 'Cerejeira é o nome dado a várias espécies de árvores frutíferas originárias da Ásia, algumas frutíferas, outras produtoras de madeira nobre.'
       , vocabulary = 'aroeira cedro cerejeira coqueiro ipê jacarandá mogno pau-brasil pinheiro seringueira'
       , errors = 2
 
-    redis.db.aroeira = [ 0 ]
-    redis.db.cedro = [ 1 ]
-    redis.db.cerejeira = [ 2 ]
-    redis.db.coqueiro = [ 3 ]
-    redis.db['ipê'] = [ 4 ]
-    redis.db['jacarandá'] = [ 5 ]
-    redis.db['mogno'] = [ 6 ]
+    redis.db.aroeira       = [ 0 ]
+    redis.db.cedro         = [ 1 ]
+    redis.db.cerejeira     = [ 2 ]
+    redis.db.coqueiro      = [ 3 ]
+    redis.db['ipê']        = [ 4 ]
+    redis.db['jacarandá']  = [ 5 ]
+    redis.db['mogno']      = [ 6 ]
     redis.db['pau-brasil'] = [ 7 ]
-    redis.db.pinheiro = [ 8 ]
-    redis.db.seringueira = [ 9 ]
+    redis.db.pinheiro      = [ 8 ]
+    redis.db.seringueira   = [ 9 ]
 
     searching(redis, query, vocabulary, errors, function (err, ids) {
       assert.deepEqual(ids, [ 2 ])
@@ -30,21 +30,21 @@ describe('searching', function () {
 
 describe('searching', function () {
   it('should match two vocabulary words', function (done) {
-    var redis      = mock.createClient()
+    var redis = mock.createClient()
       , query = 'O cedro-cheiroso (Cedrela odorata), também conhecido pelos nomes vulgares de acaju, cedro-fêmea, cedro-rosa, cedro-espanhol, cedro-vermelho, cedro-mogno e cedro-brasileiro1 é uma árvore da família das meliáceas, com uma ampla distribuição natural, ocorrendo do México a Argentina.'
       , vocabulary = 'aroeira cedro cerejeira coqueiro ipê jacarandá mogno pau-brasil pinheiro seringueira'
       , errors = 2
 
-    redis.db.aroeira = [ 0 ]
-    redis.db.cedro = [ 1 ]
-    redis.db.cerejeira = [ 2 ]
-    redis.db.coqueiro = [ 3 ]
-    redis.db['ipê'] = [ 4 ]
-    redis.db['jacarandá'] = [ 5 ]
-    redis.dbmogno = [ 6 ]
+    redis.db.aroeira       = [ 0 ]
+    redis.db.cedro         = [ 1 ]
+    redis.db.cerejeira     = [ 2 ]
+    redis.db.coqueiro      = [ 3 ]
+    redis.db['ipê']        = [ 4 ]
+    redis.db['jacarandá']  = [ 5 ]
+    redis.dbmogno          = [ 6 ]
     redis.db['pau-brasil'] = [ 7 ]
-    redis.db.pinheiro = [ 8 ]
-    redis.db.seringueira = [ 9 ]
+    redis.db.pinheiro      = [ 8 ]
+    redis.db.seringueira   = [ 9 ]
 
     searching(redis, query, vocabulary, errors, function (err, ids) {
       assert.deepEqual(ids, [ 1, 6 ])
