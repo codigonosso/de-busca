8f19215b6076c96e2c01d00ff94096ad46e8862b
Talles L <talleslasmar@gmail.com>
2015-06-28 17:22:41 -0300

    fixing mock this context issue

--- test/redis-mock.js
+++ test/redis-mock.js
@@ -1,25 +1,25 @@
 var redis = require('redis')
 
-exports.createClient = function () {
+exports.createClient = function () { return new Mock() }
 
-  return {
+function Mock () {
 
-      // the database that we'll check against
-      db: { }
+  var that = this
 
-      // making it a RedisClient
-    , __proto__: redis.RedisClient.prototype
+  // the database that we'll check against
+  this.db = { }
 
-      // our version of redis' sadd
-    , sadd: function (key, value) {
-        if (!(key in this.db)) this.db[key] = [ value ]
-        else if (this.db[key].indexOf(value) === -1) this.db[key].push(value)
-      }
-
-      // our version of redis' get
-    , get: function (key, callback) { callback(null, this.db[key]) }
-
+  // our version of redis' sadd
+  this.sadd = function (key, value) {
+    if (!(key in that.db)) that.db[key] = [ value ]
+    else if (that.db[key].indexOf(value) === -1) that.db[key].push(value)
   }
 
+  // our version of redis' get
+  this.get = function (key, callback) { callback(null, that.db[key]) }
+
 }
+
+// making it a RedisClient
+Mock.prototype = redis.RedisClient.prototype
 
