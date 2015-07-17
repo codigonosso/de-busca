8db50c215f563b7e40b7841f949de6318fe3553d
Talles L <talleslasmar@gmail.com>
2015-07-17 01:17:12 -0300

    returning something after indexing

--- test/indexing.js
+++ test/indexing.js
@@ -4,47 +4,28 @@ var assert   = require('assert')
 
 describe('indexing', function () {
 
-  it('should raise an error if no text is provided', function () {
-    var redis = mock.createClient()
-
-    assert.throws(function () { indexing(redis, 1337) })
-  })
-
-  it('should return false if there\'s nothing to index', function () {
-    var redis = mock.createClient()
-
-    assert(!indexing(redis, 1, 'Como?'))
-  })
-
   it('should index correctly', function () {
-    var redis = mock.createClient()
 
-    assert(indexing(redis, 23, '\
+    var keywords = indexing('\
 É bom\n\
 Passar uma tarde em Itapuã\n\
 Ao sol que arde em Itapuã\n\
 Ouvindo o mar de Itapuã\n\
-Falar de amor em Itapuã'))
+Falar de amor em Itapuã')
 
-    assert.deepEqual(redis.db.bom,    [ 23 ])
-    assert.deepEqual(redis.db.passar, [ 23 ])
-    assert.deepEqual(redis.db.tarde,  [ 23 ])
-    assert.deepEqual(redis.db.itapua, [ 23 ])
-    assert.deepEqual(redis.db.sol,    [ 23 ])
-    assert.deepEqual(redis.db.arde,   [ 23 ])
-    assert.deepEqual(redis.db.falar,  [ 23 ])
-    assert.deepEqual(redis.db.amor,   [ 23 ])
+    assert.deepEqual(keywords, {
+        bom:     [ 0 ]
+      , passar:  [ 1 ]
+      , tarde:   [ 2 ]
+      , itapua:  [ 3, 6, 9, 12 ]
+      , sol:     [ 4 ]
+      , arde:    [ 5 ]
+      , ouvindo: [ 7 ]
+      , mar:     [ 8 ]
+      , falar:   [ 10 ]
+      , amor:    [ 11 ]
+    })
 
-    assert.deepEqual(redis.db['23:bom'],     [ 0 ])
-    assert.deepEqual(redis.db['23:passar'],  [ 1 ])
-    assert.deepEqual(redis.db['23:tarde'],   [ 2 ])
-    assert.deepEqual(redis.db['23:itapua'],  [ 3, 6, 9, 12 ])
-    assert.deepEqual(redis.db['23:sol'],     [ 4 ])
-    assert.deepEqual(redis.db['23:arde'],    [ 5 ])
-    assert.deepEqual(redis.db['23:ouvindo'], [ 7 ])
-    assert.deepEqual(redis.db['23:mar'],     [ 8 ])
-    assert.deepEqual(redis.db['23:falar'],   [ 10 ])
-    assert.deepEqual(redis.db['23:amor'],    [ 11 ])
   })
 
 })
